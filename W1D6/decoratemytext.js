ea2ce99b0e687859c2d345d8dc5c7f823ab6605d
hewan gebrealief gebreslassie <hewan.83@gmail.com>
2019-07-30 13:57:11 -0500

    W1D6
    text docorate

---
+++ W1D6/decoratemytext.js
@@ -0,0 +1,28 @@
+
+function alrt(){
+   var x = document.getElementById("txt");
+    console.log(x.style.fontSize);
+    var fontSize = x.style.fontSize.split('p')[0];
+   x.style.fontSize = parseInt(fontSize, 10) + 2 + "pt";
+};
+function chkAlert(){
+    var x = document.getElementById("txt");
+   
+    if (chk.checked) {
+        x.style.fontWeight = "bold";
+      } else {
+        x.style.fontWeight = "normal";
+      }
+ };
+ window.onload = function () {
+     "use strict";
+
+     var bigButton = document.getElementById('big-button');
+     bigButton.onclick = function () {
+         setInterval(function(){
+            alrt();
+         },500);
+        
+     }
+     
+ };
