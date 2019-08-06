a08db7eb586d040f6ca772b0f81ca580a28ae68c
hewan gebrealief gebreslassie <hewan.83@gmail.com>
2019-08-06 18:41:00 -0500

    W3D2

--- W3D2/circles.js
+++ W3D2/circles.js
@@ -1,11 +1,31 @@
 'use strict';
 $(function (){
-    $('#circle').setInterval(sizeIncrease, 250);
-    $("#circle").on('click', disapear);
+    //setInterval(sizeIncrease, 250);
+    $("#container").on('click', '.circle', disapear);
+    $("#mybutton").on('click', newLook);
 });
 function sizeIncrease() {
-    $('#circle').css('width', width+10);
+    var width=$('.circle').width();
+    //console.log(width);
+    $('.circle').css({'width': width + 10 + 'px'});
     }
 function disapear(){
-    $('#circle').hide();
+    $('.circle').remove();
+}
+function newLook(){
+    var newWidth=$("#id1").val();
+    var grwAmount=$("#id2").val();
+    grwAmount = parseInt(grwAmount, 10);
+    var grwRate=$("#id3").val();  
+    var circleNumber=$("#id4").val(); 
+    circleNumber = parseInt(circleNumber,  10);
+     for(let i=0;i<circleNumber;i++){
+    $('#container').prepend('<div class="circle"></div>');
+     }
+   $('.circle').css({'width': newWidth + 'px'});
+    setInterval(newInterval, grwRate, grwAmount);
+}
+function newInterval( grwAmount){
+    var startingWidth = $('.circle').width();  
+      $('.circle').css({'width': startingWidth + grwAmount + 'px'});
 }
